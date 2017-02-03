e48569cf4a83ca2c0d41d7b1b535672d1c84a93b
Mika Jihoon Yoon <mika@zoyi.co>
2017-02-03 12:02:18 +0900

    For B

---
+++ lib/src/main/java/se/ichino/androidtestlibraryc/ClassForB.java
@@ -0,0 +1,10 @@
+package se.ichino.androidtestlibraryc;
+
+/**
+ * Created by mika on 2017. 2. 3..
+ */
+public class ClassForB {
+  public static String callB() {
+    return "B:" + BuildConfig.VERSION_NAME;
+  }
+}
