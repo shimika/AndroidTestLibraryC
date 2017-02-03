642a8c2a281eac3b272dab3391b7166724d77362
Mika Jihoon Yoon <mika@zoyi.co>
2017-02-03 12:01:44 +0900

    For A

---
+++ lib/src/main/java/se/ichino/androidtestlibraryc/ClassForA.java
@@ -0,0 +1,10 @@
+package se.ichino.androidtestlibraryc;
+
+/**
+ * Created by mika on 2017. 2. 3..
+ */
+public class ClassForA {
+  public static String callA() {
+    return "A:" + BuildConfig.VERSION_NAME;
+  }
+}
